91711d999e68fcc5dce005b76f31c8851750658d
Kiersten White <kierstenswhite@gmail.com>
2023-05-31 12:16:47 -0400

    Numbers in order

--- Program.cs
+++ Program.cs
@@ -24,24 +24,31 @@
 // }
 
 // EXERCISE THREE: Order these student names alphabetically, in descending order (Z to A)
-List<string> names = new List<string>()
-{
-    "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
-    "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
-    "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
-    "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
-    "Francisco", "Tre"
-};
+// List<string> names = new List<string>()
+// {
+//     "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+//     "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+//     "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+//     "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+//     "Francisco", "Tre"
+// };
 
-List<string> descend = names.OrderByDescending(n => n).ToList();
+// List<string> descend = names.OrderByDescending(n => n).ToList();
 
-foreach (string name in descend) 
-{
-    Console.WriteLine(name);
-}
+// foreach (string name in descend) 
+// {
+//     Console.WriteLine(name);
+// }
 
 // // EXERCISE FOUR: Build a collection of these numbers sorted in ascending order
-// List<int> numbers = new List<int>()
-// {
-//     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
-// };
+    List<int> numbers = new List<int>()
+        {
+            15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+        };
+
+    List<int> AscendingOrderList = numbers.OrderBy(n => n).ToList();
+
+    foreach (int num in AscendingOrderList)
+    {
+        Console.WriteLine(num);
+    }
