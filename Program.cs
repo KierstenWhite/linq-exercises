3ad5398c439622753f73c4187eee101420a4b6ad
Kiersten White <kierstenswhite@gmail.com>
2023-05-31 11:52:38 -0400

    Found L Fruits

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Find the words in the collection that start with the letter 'L'
+List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+
+IEnumerable<string> LFruits = from fruit in fruits
+where fruit.StartsWith("L")
+select fruit;
+
+foreach (string f in LFruits)
+{
+    Console.WriteLine($"{f}");
+}
