33ece029fcb20a9baf820b41f7047b97bbdd1587
Kiersten White <kierstenswhite@gmail.com>
2023-05-31 12:14:36 -0400

    Sort Student Names Z-A

--- Program.cs
+++ Program.cs
@@ -11,14 +11,37 @@
 // }
 
 // EXERCISE TWO: Which of the following numbers are multiples of 4 or 6
-List<int> numbers = new List<int>()
+// List<int> numbers = new List<int>()
+// {
+//     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+// };
+
+// IEnumerable<int> fourSixMultiples = numbers.Where(n => n % 4 == 0 || n % 6 == 0);
+
+// foreach (int num in fourSixMultiples)
+// {
+//     Console.WriteLine($"{num}");
+// }
+
+// EXERCISE THREE: Order these student names alphabetically, in descending order (Z to A)
+List<string> names = new List<string>()
 {
-    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+    "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+    "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+    "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+    "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+    "Francisco", "Tre"
 };
 
-IEnumerable<int> fourSixMultiples = numbers.Where(n => n % 4 == 0 || n % 6 == 0);
+List<string> descend = names.OrderByDescending(n => n).ToList();
 
-foreach (int num in fourSixMultiples)
+foreach (string name in descend) 
 {
-    Console.WriteLine($"{num}");
+    Console.WriteLine(name);
 }
+
+// // EXERCISE FOUR: Build a collection of these numbers sorted in ascending order
+// List<int> numbers = new List<int>()
+// {
+//     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+// };
