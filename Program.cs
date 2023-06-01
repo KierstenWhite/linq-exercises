c13861eba80c7fe5afe1e9103d2272201e713d72
Kiersten White <kierstenswhite@gmail.com>
2023-06-01 12:01:48 -0400

    Final Bank Exercise

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿// EXERCISE ONE: Find the words in the collection that start with the letter 'L'
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+// EXERCISE ONE: Find the words in the collection that start with the letter 'L'
 // List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
 
 // IEnumerable<string> LFruits = from fruit in fruits
@@ -83,31 +88,31 @@
 // Console.WriteLine(prices.Max());
 
 //EXERCISE EIGHT
-List<int> wheresSquaredo = new List<int>()
-{
-    66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
-};
+// List<int> wheresSquaredo = new List<int>()
+// {
+//     66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+// };
 
-bool isPerfectSquare(int numm) //Deciding whether or not the number is a perfect square
-{
-    int square = (int)Math.Sqrt(numm);
-    return square * square == numm;
-}
+// bool isPerfectSquare(int numm) //Deciding whether or not the number is a perfect square
+// {
+//     int square = (int)Math.Sqrt(numm);
+//     return square * square == numm;
+// }
 
-List<int> storageList = new List<int>(); //Creating a new list to store the numbers in
+// List<int> storageList = new List<int>(); //Creating a new list to store the numbers in
 
-foreach (int number in wheresSquaredo) //Once we find a perfect square, stop adding numbers to the list
-{
-    if (isPerfectSquare(number))
-        break;
+// foreach (int number in wheresSquaredo) //Once we find a perfect square, stop adding numbers to the list
+// {
+//     if (isPerfectSquare(number))
+//         break;
 
-        storageList.Add(number);
-}
+//         storageList.Add(number);
+// }
 
-foreach (int number in storageList) //Print the storage list
-{
-    Console.Write(number + " ");
-}
+// foreach (int number in storageList) //Print the storage list
+// {
+//     Console.Write(number + " ");
+// }
 /*
     Store each number in the following List until a perfect square
     is detected.
@@ -115,3 +120,69 @@ foreach (int number in storageList) //Print the storage list
     Expected output is { 66, 12, 8, 27, 82, 34, 7, 50, 19, 46 } 
     Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
 */
+
+// EXERCISE NINE: Build a collection of customers who are millionaires
+public class Customer
+{
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public string Bank { get; set; }
+}
+
+public class Bank
+{
+    public string Name { get; set;}
+}
+
+public class Millionaire {
+   public string Name { get; set; }
+   public string Bank { get; set; }
+}
+
+public class Program
+{
+    public static void Main() {
+        List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        };
+
+        List<Customer> millionaires = customers.Where(customer => customer.Balance >= 1000000).ToList();
+
+        foreach (var millionaire in millionaires)
+        {
+            Console.WriteLine($"Name: {millionaire.Name}, Bank: {millionaire.Bank}, Balance: {millionaire.Balance}");
+        }
+
+        /*
+            Given the same customer set, display how many millionaires per bank.
+            Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+            Example Output:
+            WF 2
+            BOA 1
+            FTB 1
+            CITI 1
+        */
+ 
+        var millionaireBank = customers
+                .Where(customer => customer.Balance >= 1000000)
+                .GroupBy(m => m.Bank)
+                .Select(group => new {Bank = group.Key, Count = group.Count()});
+
+        foreach (var bank in millionaireBank)
+        {
+            Console.WriteLine($"{bank.Bank} {bank.Count}");
+        }
+
+
+    }
+}
